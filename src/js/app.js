658f3a344c0e58d29eba76b62bc8de3a3fbe2cd5
Taylor Callsen <Taylor@Taylor-mbp.local>
2015-07-24 23:58:51 -0500

    including localforage

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 
 var React = require("react");
 var GeoView = require("app/GeoView");
+var localForage = require('localforage');
 
 var app = {
 
@@ -9,12 +10,33 @@ var app = {
     },
 
     onDeviceReady: function() {
+    	
+    	/*
+    	localForage.config({
+		    driver      : localForage.WEBSQL, // Force WebSQL; same as using setDriver()
+		    name        : 'myApp',
+		    version     : 1.0,
+		    size        : 4980736, // Size of database, in bytes. WebSQL-only for now.
+		    storeName   : 'keyvaluepairs', // Should be alphanumeric, with underscores.
+		    description : 'some description'
+		}); */
+
+		var key = 'STORE_KEY';
+	      var value = 'some shit from offline';
+	      var r = document.getElementById('results')
+	      localForage.setItem(key, value, function() {
+	        console.log('Saved: ' + value);
+	        localForage.getItem(key, function(err, readValue) {
+	            alert('Read: : ' + readValue);
+	        });
+	      });
 
         var mountNode = document.getElementById('reactAppContainer');
 
         React.renderComponent(GeoView(), mountNode);
 
         console.log("React should now be loaded now");
+
     }
 
 };
